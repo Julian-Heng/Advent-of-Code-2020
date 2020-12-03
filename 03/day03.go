2828d9f661ea60e135d8167daa9d1e483c4af5b7
julian-heng <julianhengwl@gmail.com>
2020-12-03 13:33:27 +0800

    Day 3

---
+++ 03/day03.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "os"
+)
+
+
+func main() {
+    lines, err := readlines("input")
+
+    if err != nil {
+        os.Exit(1)
+    }
+
+    fmt.Println(solve(lines, 3, 1))
+
+    p2 := 1
+    p2 *= solve(lines, 1, 1)
+    p2 *= solve(lines, 3, 1)
+    p2 *= solve(lines, 5, 1)
+    p2 *= solve(lines, 7, 1)
+    p2 *= solve(lines, 1, 2)
+
+    fmt.Println(p2)
+}
+
+
+func solve(lines []string, roffset int, doffset int) (int) {
+    r := 0
+    d := 0
+    trees := 0
+    for d < len(lines) {
+        if lines[d][r] == '#' {
+            trees++
+        }
+
+        r = (r + roffset) % len(lines[d])
+        d += doffset
+    }
+    return trees
+}
+
+
+func readlines(path string) ([]string, error) {
+    lines := []string{}
+
+    file, err := os.Open(path)
+
+    if err != nil {
+        return lines, err
+    }
+
+    defer file.Close()
+
+    scanner := bufio.NewScanner(file)
+    for scanner.Scan() {
+        lines = append(lines, scanner.Text())
+    }
+
+    return lines, scanner.Err()
+}
---
+++ 03/day03.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+from math import prod
+
+
+def main():
+    with open("input", "r") as f:
+        lines = f.read().strip().split("\n")
+
+    spec = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+
+    print(solve(lines, 3, 1))
+    print(prod([solve(lines, x, y) for x, y in spec]))
+
+
+def solve(lines, r_offset, d_offset):
+    r = 0
+    d = 0
+    trees = 0
+    while d < len(lines):
+        trees += 1 if lines[d][r] == "#" else 0
+        r = (r + r_offset) % len(lines[d])
+        d += d_offset
+    return trees
+
+
+if __name__ == "__main__":
+    main()
