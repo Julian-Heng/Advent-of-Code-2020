ff29897eb159a5e78d7969a8aa5cc4702acc6d9e
julian-heng <julianhengwl@gmail.com>
2020-12-06 15:12:20 +0800

    Fix order

--- 06/day06.py
+++ 06/day06.py
@@ -7,8 +7,8 @@ def main():
     with open("input", "r") as f:
         l = f.read().strip().split("\n\n")
         l = [[set(j) for j in i.splitlines()] for i in l]
-    print(sum(map(len, (reduce(set.intersection, i) for i in l))))
     print(sum(map(len, (reduce(set.union, i) for i in l))))
+    print(sum(map(len, (reduce(set.intersection, i) for i in l))))
 
 
 if __name__ == "__main__":
