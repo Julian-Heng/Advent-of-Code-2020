dcc2e0d15c2e22e9ff0fd22ea589ccb1e54923c1
julian-heng <julianhengwl@gmail.com>
2020-12-06 15:11:20 +0800

    Better day06.py

--- 06/day06.py
+++ 06/day06.py
@@ -1,28 +1,14 @@
 #!/usr/bin/env python3
 
-import string
-
-from collections import Counter
 from functools import reduce
 
 
 def main():
     with open("input", "r") as f:
-        lines = f.read().strip().split("\n\n")
-
-    print(solve_1(lines))
-    print(solve_2(lines))
-
-
-def solve_1(ln):
-    l = string.ascii_lowercase
-    return sum(map(lambda x: sum([i in x for i in l]), ln))
-
-
-def solve_2(ln):
-    c = lambda x, y: "".join(set(x).intersection(y))
-    ln = [i.splitlines() for i in ln]
-    return sum(len(i) for i in (reduce(c, i) for i in ln))
+        l = f.read().strip().split("\n\n")
+        l = [[set(j) for j in i.splitlines()] for i in l]
+    print(sum(map(len, (reduce(set.intersection, i) for i in l))))
+    print(sum(map(len, (reduce(set.union, i) for i in l))))
 
 
 if __name__ == "__main__":
