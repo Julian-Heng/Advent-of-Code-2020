8afb7551fd24a08a8c97838108562461801ed492
julian-heng <julianhengwl@gmail.com>
2020-12-06 15:15:24 +0800

    Better day06.py

--- 06/day06.py
+++ 06/day06.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python3
 
-from functools import reduce
-
 
 def main():
     with open("input", "r") as f:
         l = f.read().strip().split("\n\n")
         l = [[set(j) for j in i.splitlines()] for i in l]
-    print(sum(map(len, (reduce(set.union, i) for i in l))))
-    print(sum(map(len, (reduce(set.intersection, i) for i in l))))
+    print(sum(len(set.union(*i)) for i in l))
+    print(sum(len(set.intersection(*i)) for i in l))
 
 
 if __name__ == "__main__":
